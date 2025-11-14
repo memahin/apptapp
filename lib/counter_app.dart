95ef0b050b8860d509dc73cfedfa9c7884218a46
memahin <rafiul.mahin1@gmail.com>
2025-11-14 06:42:41 +0600

    counter app

--- lib/counter_app.dart
+++ lib/counter_app.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
 
-class CounterApp extends StatelessWidget {
+class CounterApp extends StatefulWidget {
   const CounterApp({super.key});
 
   @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+
+  int _count = 0;
+
+  void _increment(){
+    setState(() {
+      _count++;
+    });
+  }
+
+  void _decrement(){
+    setState((){
+      _count--;
+    });
+  }
+
+  void _reset(){
+    setState(() {
+      _count = 0;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(padding: EdgeInsetsGeometry.all(16),
+
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:apptapp/counter_app.dart';
 import 'package:flutter/material.dart';
 
 import 'home_screen.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomeScreen(),
+      home: const CounterApp(),
     );
   }
 }
