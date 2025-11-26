2f7b16f113b498dc7b91e8c0315f5a3fcd5b7411
memahin <rafiul.mahin1@gmail.com>
2025-11-26 13:12:02 +0600

    calculator app

--- lib/calculator_app.dart
+++ lib/calculator_app.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
 
-class CalculatorApp extends StatelessWidget {
+class CalculatorApp extends StatefulWidget {
   const CalculatorApp({super.key});
 
   @override
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  String _display = '0';
+  String _firstNumber = '';
+  String _operation = '';
+  bool _isNewNumber = true;
+
+  void _addDigital() {}
+
+  void _addDecimal() {}
+
+  void _setOperation() {}
+
+  void _calculate() {}
+
+  void _clear() {}
+
+  void _clearEntry() {}
+
+  void _changeSign() {}
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator App'),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.black.withValues(alpha: 0.5),
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(color: Colors.black.withValues(alpha: 0.3)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
